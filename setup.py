98de2c34ac9c7e5e67837a49ce9fc0a2cf52baf3
Artur Shaik <artur@shaik.link>
2020-09-30 11:06:58 +0600

    change project name
    build and install package
    
    added required libs
    
    fix requires list
    
    fixing test
    
    fix
    
    add submodules
    
    remove extra lines
    
    add missing package

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='wallabag-cli',
+    name='wallabag-client',
     use_scm_version=True,
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    url='https://github.com/apetresc/wallabag-cli',
-    author='Michael Scholz',
+    url='https://github.com/artur-shaik/wallabag-client',
+    author='Artur Shaik',
     description='A command-line client for the self-hosted \
             read-it-later app Wallabag',
     classifiers=[
@@ -18,12 +18,14 @@ setup(
     setup_requires=[
         'pytest-runner==5.1',
         'setuptools_scm==3.3.3',
-        'wheel',
     ],
     install_requires=[
-        'beautifulsoup4>=4.5.1',
+        'beautifulsoup4>=4.9.1',
         'pycryptodome>=3.9.8',
         'requests>=2.11.1',
+        'click>=7.1.2',
+        'xdg>=4.0.1',
+        'colorama>=0.4.3'
     ],
     tests_require=[
         'pytest==4.6.3',
