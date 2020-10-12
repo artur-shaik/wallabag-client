c752e26ff75b1b83f2e577bf028407aad830f874
Artur Shaik <artur@shaik.link>
2020-10-12 19:22:09 +0600

    fix custom path configuration

--- src/wallabag/api/get_api_version.py
+++ src/wallabag/api/get_api_version.py
@@ -8,6 +8,7 @@ class ApiVersion(Api):
     def __init__(self, config, custom_url=None):
         Api.__init__(self, config)
         self.custom_url = custom_url
+        self.skip_auth = True
 
     def _get_api_url(self):
         return self._build_url(ApiMethod.VERSION, self.custom_url)
